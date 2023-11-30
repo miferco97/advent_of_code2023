7d462e74e366bd2772e0ed9a93fb75b81c221b63
miferco97 <miferco97@gmail.com>
2023-11-30 22:54:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# advent_of_code_2023
+ Advent_of_code_2023 coded in RUST for learning
---
+++ day_01/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs;
+use std::process::exit;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 1 {
+        println!("Filename not provided");
+        exit(0);
+    }
+    let filename: &str = args[1].as_str();
+    let content = fs::read_to_string(filename).expect("Error reading the file");
+    println!("content:{}", content);
+}
