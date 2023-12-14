a99b998117cb3cc2697b82ec5b59c4a208371ebd
Miguel Fernandez-Cortizas <miferco97@gmail.com>
2023-12-14 17:47:57 +0100

    [day12] submit

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,17 +1,8 @@
-use regex::Regex;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::process::exit;
 
-fn encode_group(str: &str, groups: &Vec<usize>) -> String {
-    let mut string = String::from(str);
-    for elem in groups {
-        string.push_str(elem.to_string().as_str());
-        string.push_str(".");
-    }
-    string
-}
 
 #[derive(Debug)]
 struct Spring {
@@ -46,52 +37,15 @@ impl Spring {
         }
         Spring { record, groups }
     }
-    fn encode(&self) -> String {
-        encode_group(&self.record, &self.groups)
-    }
 }
 
-fn count_groups_possibilities_brute_force(
-    record: &str,
-    groups: &Vec<usize>,
-    memo_dict: &mut HashMap<String, u64>,
-) -> u64 {
-    let key = encode_group(record, groups);
-    if memo_dict.contains_key(&key) {
-        return *memo_dict.get(&key).unwrap();
+fn encode_group(str: &str, groups: &Vec<usize>) -> String {
+    let mut string = String::from(str);
+    for elem in groups {
+        string.push_str(elem.to_string().as_str());
+        string.push_str(".");
     }
-
-    // println!("spring : {:?}", orig);
-    let string = generate_string_from_groups(groups);
-    // println!("string = {string}");
-    let re = Regex::new(&string).unwrap();
-    let mut expresion: Vec<char> = record.chars().collect();
-    let mut question_positions = Vec::new();
-    let mut n_matches = 0;
-    for (index, elem) in record.chars().enumerate() {
-        if elem == '?' {
-            question_positions.push(index);
-        }
-    }
-    let n_combs = 1_u64 << question_positions.len();
-    // println!("n_combs {}",n_combs);
-    for i in 0..n_combs {
-        for (j, index) in question_positions.iter().enumerate() {
-            let new_i = i >> j;
-            if new_i % 2 == 0 {
-                expresion[*index] = '#';
-            } else {
-                expresion[*index] = '.';
-            }
-        }
-        let str_converted: String = expresion.iter().collect();
-        if re.is_match(&str_converted) {
-            n_matches += 1;
-        }
-    }
-    memo_dict.insert(key, n_matches);
-
-    n_matches
+    string
 }
 
 fn find_combs(
@@ -104,15 +58,9 @@ fn find_combs(
     // println!("{tabs}find combs: {}, groups {:?}", str_, groups);
 
     if groups.is_empty() {
-        if(str_.len() == 0 || str_.find(|x| x == '#').is_none()) {
-        return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    if !groups.is_empty() && str_.len() == 0 {
+        if str_.find(|x| x == '#').is_none() {
+            return 1;
+        } 
         return 0;
     }
 
@@ -160,20 +108,6 @@ fn find_combs(
     res
 }
 
-fn generate_string_from_groups(groups: &Vec<usize>) -> String {
-    let mut string = String::from(r"^\.*");
-    for (i, elem) in groups.iter().enumerate() {
-        for _ in 0..*elem {
-            string.push('#');
-        }
-        if i < groups.len() - 1 {
-            string.push_str(r"\.+");
-        }
-    }
-    string.push_str(r"\.*$");
-    string
-}
-
 fn parse_content(str: &str) -> Vec<Spring> {
     let mut out = Vec::new();
     let content: Vec<&str> = str.split("\n").filter(|x| !x.is_empty()).collect();
@@ -194,9 +128,8 @@ fn parse_content(str: &str) -> Vec<Spring> {
 
 fn count_matches(springs: &Vec<Spring>, memo_dict: &mut HashMap<String, u64>) -> u64 {
     let mut total_combs = 0;
-    for (i, spring) in springs.iter().enumerate() {
-        let n_matches = find_combs(&spring.record, &spring.groups, memo_dict, 0);
-        total_combs += n_matches;
+    for spring in springs {
+        total_combs += find_combs(&spring.record, &spring.groups, memo_dict, 0);
     }
     total_combs
 }
@@ -206,23 +139,15 @@ fn main() {
         println!("Filename not provided");
         exit(0);
     }
-    let mut map = HashMap::new();
 
     let filename: &str = args[1].as_str();
     let content = fs::read_to_string(filename).expect("Error reading the file");
     let springs = parse_content(&content);
-    let part1 = count_matches(&springs,&mut map);
+
+    let mut map = HashMap::new();
+    let part1 = count_matches(&springs, &mut map);
     println!("PART 1: {part1}");
-
-    // let test_spring = Spring {
-    //     record: String::from("?###????????"),
-    //     groups: vec![3, 2, 1],
-    // };
-    // let number = find_combs(&test_spring.record, &test_spring.groups, &mut map, 0);
-    // println!("number {number}");
-    // println!("number2 {}", number.pow(5));
-
     let new_springs = springs.iter().map(|x| x.unfold()).collect();
-    let part2 = count_matches(&new_springs,&mut map);
+    let part2 = count_matches(&new_springs, &mut map);
     println!("PART 2: {part2}");
 }
