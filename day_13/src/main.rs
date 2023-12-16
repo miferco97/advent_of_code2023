09e2b78d827b6dd3c54bb86b583ab6cd15bfa78a
miferco97 <miferco97@gmail.com>
2023-12-16 11:52:10 +0100

    [day13] submit

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -4,8 +4,8 @@ use std::process::exit;
 
 use day_13::*;
 
-type Discrepancy = (u32,u32);
-type Smack = [Discrepancy;2];
+type Discrepancy = (u32, u32);
+type Smack = [Discrepancy; 2];
 
 fn find_discrepancies(
     matrix: &Matrix<char>,
@@ -41,13 +41,27 @@ fn find_discrepancies(
     Some(discrepancies)
 }
 
-fn find_symmetry(matrix: &Matrix<char>, axis: u32) -> Option<u64> {
+fn changed_matrix(matrix: &Matrix<char>, discrepancy: Discrepancy) -> Matrix<char> {
+    let mut copy_matrix = matrix.clone();
+    let curr_value = matrix.at(discrepancy.0, discrepancy.1);
+    // println!("[find reflection axis] discrepancy in axis: {axis} at [{i},{j}]");
+    *copy_matrix.at_mut(discrepancy.0, discrepancy.1) = match curr_value {
+        '.' => '#',
+        '#' => '.',
+        _ => panic!("not expected character"),
+    };
+
+    copy_matrix
+}
+
+fn find_symmetry(matrix: &Matrix<char>, axis: u32) -> Vec<u64> {
     let n_dims = match axis {
         0 => matrix.rows,
         1 => matrix.cols,
         _ => panic!("axis not defined"),
     };
 
+    let mut symetries = Vec::new();
     let mut candidates: Vec<u32> = Vec::new();
 
     for i in 0..n_dims - 1 {
@@ -74,8 +88,32 @@ fn find_symmetry(matrix: &Matrix<char>, axis: u32) -> Option<u64> {
         }
 
         if valid {
-            return Some(candidate as u64 + 1);
+            symetries.push(candidate as u64 + 1);
         }
+    }
+    symetries
+}
+
+fn check_discrepancy_change(
+    axis: u32,
+    matrix: &Matrix<char>,
+    discrepancy: Discrepancy,
+) -> Option<u64> {
+    let previous_reflections = find_symmetry(matrix, axis);
+    let changed_matrix = changed_matrix(matrix, discrepancy);
+    let changed_reflections = find_symmetry(&changed_matrix, axis);
+
+    if changed_reflections.len() < 1 {
+        return None;
+    }
+    let different_reflections: Vec<&u64> = changed_reflections
+        .iter()
+        .filter(|x| !previous_reflections.contains(x))
+        .collect();
+    if different_reflections.len() == 1 {
+        return Some(*different_reflections[0]);
+    } else if different_reflections.len() > 1 {
+        panic!("more than one different reflection");
     }
     None
 }
@@ -89,7 +127,7 @@ fn find_mirrors(matrix: &Matrix<char>, axis: u32) -> Option<u64> {
         _ => panic!("axis not defined"),
     };
 
-    let mut discrepancies = Vec::new();
+    // let mut discrepancies = Vec::new();
     let mut candidates: Vec<u32> = Vec::new();
 
     for i in 0..n_dims - 1 {
@@ -100,65 +138,51 @@ fn find_mirrors(matrix: &Matrix<char>, axis: u32) -> Option<u64> {
             if discrepancy.len() > 1 {
                 continue;
             }
-            discrepancies.push(discrepancy[0]);
+            // check if the discrepancy change the symmetry if so return the value
+            // println!("axis {axis}: discrepancy: {:?}", discrepancy);
+            if let Some(t) = check_discrepancy_change(axis, matrix, discrepancy[0][0]) {
+                return Some(t);
+            }
         }
     }
-    println!("candidates: {:?}", candidates);
-    if discrepancies.len() == 0 {
-        // if no have been found search after a reflection axis and try again
-        for candidate in candidates {
-            let dist_right = n_dims - 1 - (candidate + 1);
-            let max_dist = (candidate).min(dist_right);
-            for i in (candidate - max_dist)..(candidate) {
-                let index0 = i;
-                let dist = candidate - i;
-                let index1 = candidate + 1 + dist;
-                if let Some(discrepancy) = find_discrepancies(&matrix, index0, index1, axis) {
-                    if discrepancy.len() > 1 {
-                        continue;
-                    }
-                    discrepancies.push(discrepancy[0]);
+    // println!("axis : {axis}, candidates: {:?}", candidates);
+    // if no have been found search after a reflection axis and try again
+    for candidate in candidates {
+        // for the original reflection axis
+        let dist_right = n_dims - 1 - (candidate + 1);
+        let max_dist = (candidate).min(dist_right);
+        for i in (candidate - max_dist)..(candidate) {
+            let index0 = i;
+            let dist = candidate - i;
+            let index1 = candidate + 1 + dist;
+            if let Some(discrepancy) = find_discrepancies(&matrix, index0, index1, axis) {
+                if discrepancy.len() > 1 {
+                    continue;
+                }
+                // println!("axis {axis}: discrepancy: {:?}", discrepancy);
+                if let Some(t) = check_discrepancy_change(axis, matrix, discrepancy[0][0]) {
+                    return Some(t);
                 }
             }
         }
     }
-
-    println!("discrepancies: {:?}", discrepancies);
-    for smacks in discrepancies {
-        let mut copy_matrix = matrix.clone();
-        for discrepancy in smacks{
-            println!("changed: [{},{}]", discrepancy.0, discrepancy.1);
-
-        let curr_value = matrix.at(discrepancy.0, discrepancy.1);
-        // println!("[find reflection axis] discrepancy in axis: {axis} at [{i},{j}]");
-        *copy_matrix.at_mut(discrepancy.0, discrepancy.1) = match curr_value {
-            '.' => '#',
-            '#' => '.',
-            _ => panic!("not expected character"),
-        };
-
-        println!("copy_matrix: {}", copy_matrix);
-        if let Some(t) = find_symmetry(&copy_matrix, axis) {
-            return Some(t);
-        }
-    }
-    }
-    // find_symmetry(matrix, axis)
     None
 }
 
 fn compute_mirrors(
     matrix_vec: &mut Vec<Matrix<char>>,
-    perfect_reflection: bool,
-    _last_values: &mut Vec<(u32, u32)>,
+    perfect_reflection: bool
 ) -> u64 {
     if perfect_reflection {
         let mut value = 0;
         for matrix in matrix_vec {
-            if let Some(t) = find_symmetry(matrix, 1) {
-                value += t;
-            } else if let Some(t) = find_symmetry(matrix, 0) {
-                value += 100 * t;
+            let vertical = find_symmetry(matrix, 1);
+            let horizontal = find_symmetry(matrix, 0);
+            if vertical.len() == 1 {
+                value += vertical[0];
+            }
+            if horizontal.len() == 1 {
+                value += 100 * horizontal[0];
             }
         }
         return value;
@@ -167,12 +191,20 @@ fn compute_mirrors(
     let mut value = 0;
     let mut i = 0;
     for matrix in matrix_vec {
+        let mut computed = 0;
         if let Some(t) = find_mirrors(matrix, 1) {
+            computed += 1;
             value += t;
-        } else if let Some(t) = find_mirrors(matrix, 0) {
+        }
+        if let Some(t) = find_mirrors(matrix, 0) {
+            computed += 1;
             value += 100 * t;
-        } else {
+        }
+        if computed == 0 {
             println!(" {i} no reflection {}", matrix);
+        }
+        if computed > 1 {
+            println!(" {i} more than one reflection {}", matrix);
         }
         i += 1;
     }
@@ -191,10 +223,9 @@ fn main() {
     for mat_str in content.split("\n\n") {
         matrix_vec.push(Matrix::new_from_str(mat_str));
     }
-    let mut v = vec![(2, 0); matrix_vec.len()];
-    let part1 = compute_mirrors(&mut matrix_vec, true, &mut v);
-    println!("PART_1 : {}", part1); // 27664
+    let part1 = compute_mirrors(&mut matrix_vec, true);
+    println!("PART_1 : {}", part1);
 
-    let part2 = compute_mirrors(&mut matrix_vec, false, &mut v);
-    println!("PART_2 : {}", part2); // 33991
+    let part2 = compute_mirrors(&mut matrix_vec, false);
+    println!("PART_2 : {}", part2);
 }
