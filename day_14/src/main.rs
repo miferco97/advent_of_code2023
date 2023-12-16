676874a27164e4588ba1c02604a52d4cbe7c944e
miferco97 <miferco97@gmail.com>
2023-12-16 21:16:05 +0100

    [day14] submit

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::process::exit;
 
 use day_14::*;
 
+#[derive(PartialEq, PartialOrd, Copy, Clone, Debug)]
 enum Direction {
     North,
     East,
@@ -12,12 +14,10 @@ enum Direction {
 }
 
 fn slide_vector(input: &Vec<&char>, inverse: bool) -> Vec<char> {
-    // println!("input: {:?}", input);
     let vec = match inverse {
         true => input.iter().rev().cloned().collect(),
         false => input.clone(),
     };
-    // println!("input: {:?}", vec);
     let mut out = vec.iter().map(|x| **x).collect::<Vec<char>>();
 
     let mut index = 0;
@@ -29,18 +29,8 @@ fn slide_vector(input: &Vec<&char>, inverse: bool) -> Vec<char> {
             n_items = vec.len();
         }
 
-        // println!("index : {index}, n_items {n_items}, len {}, slice: {:?}",vec.len(),&vec[index..n_items]);
-        // if index == n_items {
-        //     index += 1;
-        //     continue;
-        // }
-        let n_rocks = &vec[index..n_items]
-            .iter()
-            .filter(|x| ***x == 'O')
-            .count();
+        let n_rocks = &vec[index..n_items].iter().filter(|x| ***x == 'O').count();
         let mut n_rocks = *n_rocks;
-        // println!("n_rocks: {:?} in {:?}", n_rocks, &vec[index..n_items]);
-        // println!("n_rocks: {:?}", n_rocks);
         for i in index as usize..n_items {
             let value = vec[i];
             if value == &'#' {
@@ -57,21 +47,19 @@ fn slide_vector(input: &Vec<&char>, inverse: bool) -> Vec<char> {
         }
         index = n_items + 1;
     }
-    // println!("out:   {:?}\n", out);
     if inverse {
         out.reverse();
     }
     out
-
 }
 
 fn slide_direction(matrix: &mut Matrix<char>, direction: Direction) {
     let reverse;
     match direction {
-        Direction::North | Direction::West=> {
+        Direction::North | Direction::West => {
             reverse = false;
         }
-        Direction::South | Direction::East=> {
+        Direction::South | Direction::East => {
             reverse = true;
         }
     }
@@ -88,12 +76,12 @@ fn slide_direction(matrix: &mut Matrix<char>, direction: Direction) {
                 let vec = copy_matrix.col(j);
                 let vec = slide_vector(&vec, reverse);
                 matrix.substitute_col(j, vec);
-            },
+            }
             Direction::East | Direction::West => {
                 let vec = copy_matrix.row(j);
                 let vec = slide_vector(&vec, reverse);
                 matrix.substitute_row(j, vec);
-            },
+            }
         };
     }
 }
@@ -110,8 +98,42 @@ fn compute_load(matrix: &Matrix<char>) -> u64 {
     load
 }
 
+fn hash_matrix(matrix: &Matrix<char>) -> String {
+    matrix.data.iter().collect()
+}
 
+fn cycle_matrix(matrix: &mut Matrix<char>, n_times: Option<usize>) -> (usize, usize) {
+    let cycle = [
+        Direction::North,
+        Direction::West,
+        Direction::South,
+        Direction::East,
+    ];
 
+    let mut dict: HashMap<String, usize> = HashMap::new();
+    let mut n_changes = 0;
+
+    loop {
+        for dir in cycle.iter() {
+            slide_direction(matrix, *dir);
+        }
+        n_changes += 1;
+
+        if let None = n_times {
+            // infinite case
+            let hash = hash_matrix(&matrix);
+            if let Some(value) = dict.get(&hash) {
+                return (*value, n_changes);
+            } else {
+                dict.insert(hash.clone(), n_changes);
+            }
+        } else {
+            if n_changes >= n_times.unwrap() {
+              return (0, n_changes);
+            }
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -123,15 +145,17 @@ fn main() {
     let filename: &str = args[1].as_str();
     let content = fs::read_to_string(filename).expect("Error reading the file");
     let mut matrix = Matrix::new_from_str(&content);
+
+    // TEST
     let mut part1_matrix = matrix.clone();
+    let mut part2_matrix = matrix.clone();
+
     slide_direction(&mut part1_matrix, Direction::North);
     println!("PART 1: {}", compute_load(&part1_matrix));
 
-    println!("Matrix: {}", matrix);
-    println!("Matrix: {}", matrix);
-    slide_direction(&mut matrix, Direction::West);
-    println!("Matrix: {}", matrix);
-    // println!("Matrix 2: {}", matrix);
-
-    // println!("PART 2: {part2}");
+    let (begin, n_changes) = cycle_matrix(&mut matrix, None);
+    let period = n_changes - begin ;
+    let n_iters = begin + (1000000000-begin) % period;
+    cycle_matrix(&mut part2_matrix, Some(n_iters));
+    println!("PART 2: {}", compute_load(&part2_matrix)); 
 }
